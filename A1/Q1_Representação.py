5209e9fa11386ca04a9926942070dee525934f38
TheValduga <lukinhas.valduga@gmail.com>
2023-04-16 00:12:55 -0300

    terminado questao 1

---
+++ A1/Q1_Representação.py
@@ -0,0 +1,76 @@
+# Questão 1
+
+class Graph():
+    def __init__(self) -> None:
+        self.adjList = {} # lista de adjacencias
+        self.numEdges = 0
+    
+    def GetVerticesQuantity(self) -> int:
+        return len(self.adjList)
+    
+    def GetEdgesQuantity(self) -> int:
+        return self.numEdges
+    
+    def GetDegree(self, vertex: str) -> int:
+        return len(self.adjList[vertex]['neighborhood'])
+    
+    def GetLabel(self, index: int) -> str:
+        for v in self.adjList.items():
+            if v[1]['index'] == index:
+                return v[0]
+                
+    def GetNeighborhood(self, vertex: str) -> list:
+        return list(self.adjList[vertex]['neighborhood'].keys()) 
+    
+    def VerifyEdge(self,vertexU: str,vertexV: str) -> bool:
+        if vertexV in self.adjList[vertexU]['neighborhood']:
+            return True
+        else:
+            return False
+    
+    def GetWeight(self,vertexU: str,vertexV: str) -> float:
+        try:
+            weight = self.adjList[vertexU]['neighborhood'][vertexV]
+        
+        except:
+            weight = float('inf')
+            
+        return weight
+    
+    def read(self, file) -> None:
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            n = int(lines[0].split()[1]) # numero de vertices
+            
+            # Lendo vertices
+            # Usado o numero de vertices para determinar o laço
+            for i in range(1, n+1): 
+                vertexLabel = lines[i].split()[1]
+                
+                # dicionario idenficado pelo rotulo do vertice, armazena sua vizinhança e seu index
+                self.adjList.update({vertexLabel: {'neighborhood': {} , 'index': i}})
+            
+            # Lendo arestas
+            # Lendo array de linhas do arquivo a partir de n+2
+            # n+2 = numero de vertices + linhas com "*vertices" "*arestas"
+            for line in lines[n+2:]: #
+                self.numEdges += 1 # contando o numero de arestas
+                
+                vertexU, vertexV, weight = line.split()
+                weight = float(weight)
+                
+                # dicionario de vizinhança, armazena o rotulo do vertice vizinho e o peso da aresta
+                self.adjList[vertexU]['neighborhood'].update({vertexV: weight}) 
+                self.adjList[vertexV]['neighborhood'].update({vertexU: weight})
+                
+if __name__ == '__main__':
+    g = Graph()
+    g.read('graph_test.txt')
+    print(g.adjList)
+    print(g.GetVerticesQuantity())
+    print(g.GetEdgesQuantity())
+    print(g.GetDegree('a'))
+    print(g.GetLabel(4))
+    print(g.GetNeighborhood('c'))
+    print(g.VerifyEdge('a','c'))
+    print(g.GetWeight('a','f'))
--- A1/atividade_1.py
+++ A1/atividade_1.py
@@ -3,57 +3,58 @@ class Graph():
     def __init__(self, file):
         self.vertex = {}
         self.edge = []
-        
+
         self.read_file(file)
-    
+
     def read_file(self, open_file):
-        file = open(open_file,"r")
-        
+        file = open(open_file, "r")
+
         edges = -1
         for line in file:
             if "*" in line:
                 edges += 1
-            
+
             elif not edges:
                 vertex, label = line.split()
-                vertex = int(vertex)
-                self.vertex.update({vertex: {"label": label, "edge index": []}})
+                vertex = vertex
+                self.vertex.update({vertex: {"label": label, "edgeIndex": []}})
 
             elif edges:
                 vertex, neighbor, weight = line.split()
-                vertex = int(vertex)
-                neighbor = int(neighbor)
+                vertex = vertex
+                neighbor = neighbor
                 weight = float(weight)
-                
+
                 size = len(self.edge)
 
-                neighbor_index = self.vertex.get(neighbor).get("edge index")
+                neighbor_index = self.vertex[neighbor]["edgeIndex"]
                 neighbor_index.append(size)
-                self.vertex.get(neighbor).update({vertex: weight, "edge index": neighbor_index})
+                self.vertex.get(neighbor).update(
+                    {vertex: weight, "edgeIndex": neighbor_index})
 
-                vertex_index = self.vertex.get(vertex).get("edge index")
+                vertex_index = self.vertex[vertex]['edgeIndex']
                 vertex_index.append(size)
-                self.vertex.get(vertex).update({neighbor: weight, "edge index": vertex_index})
+                self.vertex.get(vertex).update(
+                    {neighbor: weight, "edgeIndex": vertex_index})
                 self.edge.append((vertex, neighbor, weight))
 
     def get_vertex_quantity(self) -> int:
         return len(self.vertex.keys())
-    
+
     def get_edge_quantity(self) -> int:
         return len(self.edge)
-    
+
     def get_degree(self, vertex) -> int:
         return (len(self.vertex.get(vertex)) - 2)
-    
+
     def get_label(self, vertex) -> str:
         return self.vertex.get(vertex).get("label")
-    
+
     def get_neighbor(self, vertex):
-        return      #ver aqui
+        return  # ver aqui
 
     def get_edges(self, vertex_u, vertex_v) -> bool:
         return vertex_v in self.vertex.get(vertex_u)
-    
+
     def get_weight(self, vertex_u, vertex_v) -> int:
-        return self.vertex.get(vertex_u).get(vertex_v) #ver aqui
-               
+        return self.vertex.get(vertex_u).get(vertex_v)  # ver aqui
