80c8b3272ab08d8ec0791151055ed235ede246c3
leoluizGO <leoluizoliveira123@gmail.com>
2023-04-22 02:09:32 -0300

    q3 finalizada

--- A1/Q3_CicloEuleriano.py
+++ A1/Q3_CicloEuleriano.py
@@ -1,50 +1,31 @@
 # Questão 3
-
 from Q1_Representacao import Graph
-
 
 
 def CicloEuleriano(graph: Graph):
     C = [] # Vetor de arestas visitadas
     i = False
-    j = 0
 
     for _ in range(graph.GetEdgesQuantity()):
         C.append(False)
 
-    while i == False:
-        v = j  # Vertice arbitrario
-        j += 1
+    flag_vertex = False
+    for i in range(graph.GetVerticesQuantity()+1):      #for que verifica se o grafo é um ciclo euleriano (todos os vértices com grau par) e já busca um vértice que contenha ligação (no caso é pego o primeiro vértice que contenha ligação)
+        if i > 0: 
+            if graph.GetDegree(graph.GetLabel(i)) % 2 != 0:
+                return [False, None]
+            if graph.GetNeighborhood(graph.GetLabel(i)) != [] and flag_vertex == False:
+                v = i
+                flag_vertex = True
 
-        # Escolha arbitraria do vertice que possui alguma aresta
-        if graph.GetNeighborhood(graph.GetLabel(v)) != []:
-            i = True
-        if j > graph.GetVerticesQuantity():
-            return [False, None]
+    cycle = buscarSubcicloEuleriano(graph, v, C)    #chama a função buscarsubciclo
 
-    r, cycle, C = buscarSubcicloEuleriano(graph, v, C)
-
-    if r == False:
-        return [False, None]
-
-    else:
-        condition = False
-
-        for edge in C:
-            if edge == False:
-                condition == True
-                break
-
-        if condition:
-            return (False, None)
-
-        else:
-            return (True, cycle)
+    return [True, cycle]
 #falta verificação
 
 def buscarSubcicloEuleriano(graph: Graph, v: int, C: list) -> list:
     cycleS = [v] # Ciclo começando em v
-    cycleT = []
+    cycleT = [] # pilha que retornará o ciclo
     Cindex = 0
 
     u = v
@@ -55,30 +36,36 @@ def buscarSubcicloEuleriano(graph: Graph, v: int, C: list) -> list:
 
         for i in range(len(neighbors)):
             neighbor = graph.GetIndex(neighbors[i])
-            if (u, neighbor) or (neighbor, u) not in C:  
-                C[Cindex] = (u, neighbor)   #representação das aresta é uma lista com dois vértices
+            coord = [u, neighbor]
+            coordinv = [neighbor, u]    # é comparado se tanto o caminho de ida de um vertice a outro como o caminho de volta
+            if (coord not in C) and (coordinv not in C):  # se não estiver na lista de arestas (C) é posto e adicionado o próximo vértice a pilha S
+                C[Cindex] = (coord)   #representação das aresta é uma lista com dois vértices
                 cycleS.append(neighbor)     #adiciona a pilha S
-
                 u = neighbor
                 Cindex += 1
                 break
 
-            elif i == len(neighbors):
-                cycleT.append(cycleS.pop())     #remove pilha S e adiciona na pilha T
-                u = cycleS(len(cycleS)-1)       #o vértice atual se torna a última posição acessada antes do ultim o vertice retirado, ou seja, vira o topo da pilha.
-                #j = False
+            elif i == len(neighbors)-1:
+                x = cycleS.pop()
+                cycleT.append(x)     #remove pilha S e adiciona na pilha T
+                if cycleS != []:
+                    u = cycleS[len(cycleS)-1]       #o vértice atual se torna a última posição acessada antes do ultim o vertice retirado, ou seja, vira o topo da pilha.
 
-        if  not False in C:                     #se não tiver nenhum False no C acaba
+        if False in C or cycleS != []:                     #se não tiver nenhum False no C e a lista S estiver vazia acaba
+            j = True
+        else: 
             j = False
-
-
-"""    t = v
     
-    while True:
-        
-        
-        
-        if v == t:
-            break
-    
-"""
+    return cycleT
+
+if __name__ == '__main__':
+    g = Graph()
+    g.Read('GraphTest2.txt')
+    cycle, cycleList = CicloEuleriano(g)
+
+    if cycle: 
+        print("1")
+        cycleListStr = [str(a) for a in cycleList]
+        print(",".join(cycleListStr))
+    else:
+        print("0")
