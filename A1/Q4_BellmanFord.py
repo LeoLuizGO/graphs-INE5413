5d07794039f33fabeea69d3b7e1edecea465a230
TheValduga <lukinhas.valduga@gmail.com>
2023-04-22 13:58:47 -0300

    bellmanFord finalizado, adicionado GraphTest3

--- A1/Q4_BellmanFord.py
+++ A1/Q4_BellmanFord.py
@@ -1,46 +1,76 @@
 # Questão 4
 
-
-# VERIFICAR PQ A SAIDA TA ERRADA, OS DADOS NAO TAO SENDO GRAVADOS NA LISTA D E NA LISTA A
-
 from Q1_Representacao import Graph
 
+
 def BellmanFord(graph: Graph, s: int) -> tuple():
-    D = [] # Lista de pesos dos caminhos ( s , v )
-    A = [] # Lista de antecessores
-    
+    D = []  # Lista de pesos dos caminhos ( s , v )
+    A = []  # Lista de antecessores
+
     # Inicialização
     for i in range(graph.GetVerticesQuantity()):
         D.append(float('inf'))
         A.append(None)
-    
+
     D[s - 1] = float(0)
-    
+
     # Relaxamento
     for _ in range(graph.GetVerticesQuantity() - 1):
-        for vertex in range(1, graph.GetVerticesQuantity() + 1):
+        for vertex in range(1, graph.GetVerticesQuantity() + 1):  # Vertice
+            # Vizinho
             for neighbor in graph.GetNeighborhood(graph.GetLabel(vertex)):
-                if D[vertex-1] > D[vertex-1] + graph.GetWeight(graph.GetLabel(vertex), neighbor):
-                    D[vertex-1] = D[graph.GetIndex(neighbor)] + graph.GetWeight(graph.GetLabel(vertex), neighbor)
-                    A[vertex-1] = graph.GetIndex(neighbor)
-                    
-    # Verificação de ciclos negativos 
+                # se a distancia até o vizinho do vertice atual do laço for maior do que a distancia
+                # do vertice atual mais a aresta que leva a esse vizinho a condição é True
+                if D[graph.GetIndex(neighbor) - 1] > D[vertex-1] + graph.GetWeight(graph.GetLabel(vertex), neighbor):
+                    D[graph.GetIndex(neighbor) - 1] = D[vertex - 1] + \
+                        graph.GetWeight(graph.GetLabel(vertex), neighbor)
+                    A[graph.GetIndex(neighbor) - 1] = vertex
+
+    # Verificação de ciclos negativos
     for vertex in range(1, graph.GetVerticesQuantity() + 1):
         for neighbor in graph.GetNeighborhood(graph.GetLabel(vertex)):
-            if D[vertex - 1] + graph.GetWeight(graph.GetLabel(vertex), neighbor) < D[vertex - 1]:
+            if D[graph.GetIndex(neighbor) - 1] > D[vertex-1] + graph.GetWeight(graph.GetLabel(vertex), neighbor):
                 return (False, None, None)
-            
+
     return (True, D, A)
 
+
 if __name__ == "__main__":
-    g = Graph()
-    g.Read('GraphTest1.txt')
-    cycle, D, A = BellmanFord(g, 1)
-    if cycle:
-        print(f'D = {D}')
-        print(f'A = {A}\n')
-    
+    g1 = Graph()
+    g2 = Graph()
+    g1.Read('GraphTest1.txt')  # grafo sem ciclos negativos
+    g2.Read('GraphTest3.txt')  # grafo com ciclos negativos
+    cycle1, D1, A1 = BellmanFord(g1, 6)
+    cycle2, D2, A2 = BellmanFord(g2, 1)
+
+    if cycle1:
+        print('\nGrafo 1 não tem ciclo negativo\n')
+        print(f'D1 = {D1}')
+        print(f'A1 = {A1}\n')
+        for i in range(1, g1.GetVerticesQuantity() + 1):
+                path = []
+                vertex = i
+                while vertex is not None:
+                    path.insert(0, vertex)
+                    vertex = A1[vertex - 1]
+                path_str = ','.join(str(v) for v in path)
+                print(f"{i}: {path_str}; d={D1[i-1]}")
+
     else:
-        print('Tem ciclo negativo')
-    
-    
+        print('\nGrafo 1 tem ciclo negativo\n')
+
+    if cycle2:
+        print("\nGrafo 2 não tem ciclo negativo\n")
+        print(f'D2 = {D2}')
+        print(f'A2 = {A2}\n')
+        for i in range(1, g2.GetVerticesQuantity() + 1):
+                path = []
+                vertex = i
+                while vertex is not None:
+                    path.insert(0, vertex)
+                    vertex = A2[vertex - 1]
+                path_str = ','.join(str(v) for v in path)
+                print(f"{i}: {path_str}; d={D2[i-1]}")
+
+    else:
+        print('\nGrafo 2 tem ciclo negativo\n')
