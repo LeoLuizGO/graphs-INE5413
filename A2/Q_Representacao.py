0cc5ab6d4d093bbaae36e106f88daae62c2cfd97
leoluizGO <leoluizoliveira123@gmail.com>
2023-05-15 00:20:52 -0300

    atividade1 pronto

--- A2/Q_Representacao.py
+++ A2/Q_Representacao.py
@@ -6,10 +6,11 @@
 class Graph():
     def __init__(self) -> None:
         self.adjList = {} # lista de adjacencias
-        self.arcTrans = {}
         self.numEdges = 0
+        self.labels = []
+        self.arcTrans = {}
         self.directed = False
-    
+
     def GetVerticesQuantity(self) -> int:
         return len(self.adjList)
     
@@ -20,12 +21,12 @@ class Graph():
         return len(self.adjList[vertex]['neighborhood'])
     
     def GetLabel(self, index: int) -> str:
-        for v in self.adjList.items():
-            if v[1]['index'] == index:
-                return v[0]
+        return self.labels[index - 1]
     
     def GetIndex(self, label: str) -> int:
-        return self.adjList[label]['index']
+        for i in range(len(self.labels)):
+            if label == self.labels[i]:
+                return i+1
     
     def GetNeighborhood(self, vertex: str) -> list:
         return list(self.adjList[vertex]['neighborhood'].keys()) 
@@ -49,48 +50,50 @@ class Graph():
         return weight
     
     def Read(self, file) -> None:
-        with open(file, 'r') as f:
+        with open(file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             n = int(lines[0].split()[1]) # numero de vertices
             
             # Lendo vertices
             # Usado o numero de vertices para determinar o laço
             for i in range(1, n+1): 
-                vertexLabel = lines[i].split()[1]
+                vertexLabel = lines[i].split()[1:]
+                vertexLabel = ' '.join(vertexLabel)
+                self.labels.insert(i, vertexLabel)
                 
                 # dicionario idenficado pelo rotulo do vertice, armazena sua vizinhança e seu index
-                self.adjList.update({vertexLabel: {'neighborhood': {} , 'index': i}})
-                self.arcTrans.update({vertexLabel: {'neighborhood': {} , 'index': i}})
+                self.adjList.update({vertexLabel: {'neighborhood': {}}})
+                self.arcTrans.update({vertexLabel: {'neighborhood': {}}})
             
+            if "*arcs" in lines[n+1]: 
+                self.directed = True
             # Lendo arestas
             # Lendo array de linhas do arquivo a partir de n+2
             # n+2 = numero de vertices + linhas com "*vertices" "*arestas"
-            if "*arcs" in lines[n+1]: 
-                self.directed = True
-
             for line in lines[n+2:]: #
                 self.numEdges += 1 # contando o numero de arestas
                 
                 vertexU, vertexV, weight = line.split()
+                vertexU = int(vertexU)
+                vertexV = int(vertexV)
                 weight = float(weight)
                 
                 # dicionario de vizinhança, armazena o rotulo do vertice vizinho e o peso da aresta
-                self.adjList[vertexU]['neighborhood'].update({vertexV: weight}) 
-                self.arcTrans[vertexV]['neighborhood'].update({vertexU: weight})
-
+                self.adjList[self.GetLabel(vertexU)]['neighborhood'].update({self.GetLabel(vertexV): weight}) 
+                self.arcTrans[self.GetLabel(vertexV)]['neighborhood'].update({self.GetLabel(vertexU): weight})
+                
                 if not(self.directed):
-                    self.adjList[vertexV]['neighborhood'].update({vertexU: weight})
-
+                    self.adjList[self.GetLabel(vertexV)]['neighborhood'].update({self.GetLabel(vertexU): weight})
                 
 if __name__ == '__main__':
     g = Graph()
-    g.Read('GraphTest2.txt')
+    g.Read('fln_pequena.net')
     print(g.adjList)
     print(g.GetVerticesQuantity())
     print(g.GetEdgesQuantity())
-    print(g.GetDegree('a'))
-    print(g.GetIndex('c'))
+    print(g.GetDegree('"Ponte"'))
+    print(g.GetIndex('"Praia Mole"'))
     print(g.GetLabel(4))
-    print(g.GetNeighborhood('c'))
-    print(g.VerifyEdge('a','c'))
-    print(g.GetWeight('i','j'))
+    print(g.GetNeighborhood('"Universidade Federal de Santa Catarina"'))
+    print(g.VerifyEdge('"Universidade Federal de Santa Catarina"','"Parque Municipal do Córrego Grande"'))
+    print(g.GetWeight('"Lagoa da Conceição"','"Elevado Rio Tavares"'))
