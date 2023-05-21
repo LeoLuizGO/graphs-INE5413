6d85476c32f0524ff45690de1930400133f634b9
TheValduga <lukinhas.valduga@gmail.com>
2023-05-20 23:20:00 -0300

    adicionado comentarios a Q2

--- A2/Q2_OrdenacaoTopologica.py
+++ A2/Q2_OrdenacaoTopologica.py
@@ -5,43 +5,46 @@
 
 from Q_Representacao import Graph
 
-def OrdenacaoTopologica (graph: Graph) -> list:
-    C = [] # vértices visitados
-    T = [] # tempo inicial do vertice
-    F = [] # tempo final do vertice
-    
+
+def OrdenacaoTopologica(graph: Graph) -> list:
+    C = []  # Vértices visitados
+    T = []  # Tempo inicial do vertice
+    F = []  # Tempo final do vertice
+
     for _ in range(graph.GetVerticesQuantity()):
         C.append(False)
         T.append(float('inf'))
         F.append(float('inf'))
-        
-    time = 0 # tempo inicial
-    O = [] # Ordenação topológica
-    
+
+    time = 0  # Tempo inicial
+    O = []  # Vetor de ordenação topológica
+
     for u in range(graph.GetVerticesQuantity()):
         if C[u] == False:
-            dfsVisitOT(graph, u, C, T ,F, time, O)
-            
+            dfsVisitOT(graph, u, C, T, F, time, O)
+
     return O
 
-def dfsVisitOT (graph: Graph, v: int, C: list, T: list, F: list, time: int, O: list) -> None:
-    C[v] = True
-    time += 1
-    T[v] = time
-    
+
+def dfsVisitOT(graph: Graph, v: int, C: list, T: list, F: list, time: int, O: list) -> None:
+    C[v] = True  # Marca vértice como visitado
+    time += 1  # Incrementa o tempo
+    T[v] = time  # Salva o tempo inicial do vértice atual
+
     for neighbor in graph.GetNeighborhood(graph.GetLabel(v + 1)):
         if C[graph.GetIndex(neighbor) - 1] == False:
-            dfsVisitOT(graph, (graph.GetIndex(neighbor)-1), C, T ,F, time, O)
-            
-    time += 1
-    F[v] = time
-    O.insert(0, graph.GetLabel(v + 1))
-    
+            dfsVisitOT(graph, (graph.GetIndex(neighbor)-1), C, T, F, time, O)
+
+    time += 1  # Incrementa o tempo
+    F[v] = time # Salva o tempo final do vértice atual
+    O.insert(0, graph.GetLabel(v + 1)) # Insere o vértice atual no inicio da ordenação
+
+
 if __name__ == "__main__":
     g = Graph()
     g.Read("manha.net")
     O = OrdenacaoTopologica(g)
-    
+
     for vertex in range(len(O)):
         print(f'{O[vertex]}', end='')
         if vertex < len(O) - 1:
